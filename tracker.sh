1159821836186c779d89e110ae13e70a8de4d64b
MilkLotion <mertiz36@naver.com>
2021-09-24 12:24:43 +0900

    09.24 12:30 update - setting optimize

--- tracker.sh
+++ tracker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo sh -c 'echo 100 > /sys/devices/pwm-fan/target_pwm'
+sudo apt-get upgrade
+sudo apt-get update
+pip install -r requirements.txt
 # client system execute
 python3 main.py;
